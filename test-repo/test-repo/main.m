a236fbb873c3d270f57ce377f44fa4eb4f21efb4
Alexey Vinokurov <vinokurov.alexey@gmail.com>
2011-07-30 23:08:29 +0400

    test-repo first commit

---
+++ test-repo/test-repo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test-repo
+//
+//  Created by Alexey Vinokurov on 7/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
